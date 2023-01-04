28676f1a086e46b189f89fed9e31116dc1944f98
shunphoenix55 <76583147+shunphoenix55@users.noreply.github.com>
2023-01-04 12:23:05 +0530

    Setup App.js

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+require("dotenv").config({path: "./config.env"});
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/routes"))
+
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+    dbo.connectToServer( (err) => {
+        if (err) console.error(err);
+    });
+    console.log(`Server is running on localhost/${port}`);
+})
